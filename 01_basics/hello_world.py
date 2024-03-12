6ec1517c6e57e717f64669c724324c407b399367
Parag0712 <paragvadgama123@gmail.com>
2024-03-12 23:10:12 +0530

    mutable and immutable.py

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,5 @@
+print("chai aur python");
+def chai():
+    print("Hello"); 
+
+chai()
---
+++ 01_basics/mutable_immutable.py
@@ -0,0 +1,10 @@
+# mutable => can be change
+    # List, Set, Dictionary, Array
+# Immutable => cannot be changed
+    #  Integers, Boolean, Strings, Tuples, Bytes ,Floats Frozen Set
+x=10
+y=x
+print(x) #10
+x=20
+print(x) #20
+print(y) #10
