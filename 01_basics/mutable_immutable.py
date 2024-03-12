32e691720d356a42e7b992250232a923f720979a
Parag0712 <paragvadgama123@gmail.com>
2024-03-12 23:10:14 +0530

    mutable and immutable.py

--- 01_basics/mutable_immutable.py
+++ 01_basics/mutable_immutable.py
@@ -2,6 +2,7 @@
     # List, Set, Dictionary, Array
 # Immutable => cannot be changed
     #  Integers, Boolean, Strings, Tuples, Bytes ,Floats Frozen Set
+# https://app.eraser.io/workspace/XxDVsGXDuJvdc8PW77pp
 x=10
 y=x
 print(x) #10
